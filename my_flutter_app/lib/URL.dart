9728c86c222fc86de4431c657009748be47f12c4
Devin-Hasnaka <e20385@eng.pdn.ac.lk>
2024-09-14 02:02:30 +0530

    Login page connected with back end
    connected signup page, login with google with backend.
    todo: get the token and store it in the frontend

--- my_flutter_app/lib/URL.dart
+++ my_flutter_app/lib/URL.dart
@@ -1,4 +1,66 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:my_flutter_app/model/hospitalModel.dart';
+
 class URL {
-  static const String BASE_URL = "http://localhost:5000/api";
-  
+  static const String BASE_URL = "http://10.0.2.2:8080/api";
+}
+
+// /user/self/hospitals
+Future<List<String>> hospitallist() async {
+  const url = URL.BASE_URL + "/user/self/hospitals";
+  final uri = Uri.parse(url);
+  final response = await http.get(uri);
+  final body = response.body;
+  final List<dynamic> json = jsonDecode(body);
+
+  List<String> hospitalsNames = [];
+
+  for (var hospital in json) {
+    String name = hospital['name'] ?? 'Unknown Hospital';
+    hospitalsNames.add(name);
+  }
+
+  return hospitalsNames;
+}
+
+// /api/auth/signup
+Future<int> signup(String email, String username, String phoneNumber,
+    String hospital, String registration) async {
+  const url = URL.BASE_URL + "/auth/signup";
+  final uri = Uri.parse(url);
+  final response = await http.post(
+    uri,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+      'username': username,
+      'contact_no': phoneNumber,
+      'hospital': hospital,
+      'reg_no': registration,
+      'designation': 'Dr.',
+    }),
+  );
+
+  return response.statusCode;
+}
+
+// /api/auth/verify
+Future<int> verify(String email) async {
+  const url = URL.BASE_URL + "/auth/verify";
+  final uri = Uri.parse(url);
+  final response = await http.post(
+    uri,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+    }),
+  );
+
+  return response.statusCode;
 }
