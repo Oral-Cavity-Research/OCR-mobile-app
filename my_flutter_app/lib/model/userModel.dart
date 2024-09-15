32c7e811a455b02a49ec5fbe085a2ec9f670239d
Devin-Hasnaka <e20385@eng.pdn.ac.lk>
2024-09-15 12:21:12 +0530

    verification, log out, and connecting about us page

---
+++ my_flutter_app/lib/model/userModel.dart
@@ -0,0 +1,32 @@
+class Usermodel {
+  final String username;
+  final String email;
+  final String hospital;
+  final String password;
+  final String availability;
+  final String role;
+  final String createdAt;
+  final String updatedAt;
+  final String available;
+  final String reg_no;
+  final String designation;
+  final String contact_no;
+  final String id;
+  Usermodel({
+    required this.username,
+    required this.email,
+    required this.hospital,
+    required this.password,
+    required this.availability,
+    required this.role,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.available,
+    required this.reg_no,
+    required this.designation,
+    required this.contact_no,
+    required this.id,
+  });
+}
+
+ 
--- my_flutter_app/lib/pages/auth/route_generator.dart
+++ my_flutter_app/lib/pages/auth/route_generator.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/pages/add_methods/add_role_Page.dart' as AddRolePage;
+import 'package:my_flutter_app/pages/aboutUs/about_us.dart';
+import 'package:my_flutter_app/pages/add_methods/add_role_Page.dart'
+    as AddRolePage;
 import 'package:my_flutter_app/pages/auth/google_sign.dart';
 import 'package:my_flutter_app/pages/auth/Signup_page.dart';
+import 'package:my_flutter_app/pages/home/home_page_1.dart';
 import 'package:my_flutter_app/pages/home/splash_screen.dart';
 
 class RouteGenerator {
@@ -27,9 +30,14 @@ class RouteGenerator {
         return _errorRoute();
 
       case '/add_a_role':
-        return MaterialPageRoute(builder: (_) => const AddRolePage.AddRole(onTap: null));
-      
+        return MaterialPageRoute(
+            builder: (_) => const AddRolePage.AddRole(onTap: null));
 
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+
+      case '/about_us':
+        return MaterialPageRoute(builder: (_) => const AboutUsPage());
 
       default:
         // If there is no such named route in the switch statement, e.g. /third
